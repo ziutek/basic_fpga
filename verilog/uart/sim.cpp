5c3162ab6b646da36a1f3a120315111f8bfb93d6
Michał Derkacz <michal@Lnet.pl>
2020-08-10 19:49:44 +0200

    uart...

--- verilog/uart/sim.cpp
+++ verilog/uart/sim.cpp
@@ -3,14 +3,43 @@
 #include "verilated.h"
 #include "obj_dir/Vtop.h"
 
+void ticks(Vtop *tb, int n) {
+	for (int i = 0; i < n; i++) {
+		tb->clk = i & 1;
+		tb->eval();
+		printf("%d  %d %d\n", tb->clk, tb->rx, tb->state);
+	}
+	printf("--\n");
+}
+
+void uart(Vtop *tb, char *tx) {
+	for (int n = 0; tx[n] != '\0'; n++) {
+		// start bit
+		tb->rx = 0;
+		ticks(tb, 16);
+
+		// data bits
+		for (int i = 0; i < 8; i++) {
+			tb->rx = (tx[n]>>i) & 1;
+			ticks(tb, 16);
+		}
+
+		// stop bit
+		tb->rx = 1;
+		ticks(tb, 16);
+
+		printf("char: %c\n", tb->out);
+	}
+}
+
 int main(int argc, char **argv) {
 	Verilated::commandArgs(argc, argv);
 	Vtop *tb = new Vtop;
 
-	for (int k = 0; k < 20; k++) {
-		tb->clk50 = k & 1;
-		tb->uartRx = (k > 10);
-		tb->eval();
-		printf("k=%2d, clk50=%d, uartTx=%d\n", k, tb->clk50, tb->uartTx);
-	}
+	tb->rx = 1;
+	ticks(tb, 10);
+
+	char tx[] = "$1";
+
+	uart(tb, tx);
 }
--- verilog/uart/top.v
+++ verilog/uart/top.v
@@ -1,20 +1,47 @@
 `default_nettype none
 
 module top(
-	input  wire clk50,
-
-	input  wire uartRx,
-	output reg  uartTx,
-
-	input  wire btn0,
-
-	output wire led0,
-	output wire led1
+	input wire clk,
+	input wire rx,
+	output reg [7:0] out,
+	output reg [1:0] state
 );
 
-	assign led0 = btn0 ? uartTx : 1;
-	assign led1 = btn0 ? 1 : uartTx;
+initial state = idle;
 
-	always @(posedge clk50) uartTx <= uartRx;
+localparam idle = 0;
+localparam start = 1;
+localparam data = 2;
+localparam stop = 3;
+
+//reg [1:0] state = idle;
+reg [3:0] subCnt;
+reg [3:0] bitCnt;
+reg       carry;
+
+always @(posedge clk) begin
+	{carry, subCnt} <= subCnt + 1'b1;
+	case (state)
+	idle:
+		if (rx == 0) begin
+			subCnt <= 8;
+			state <= start;
+		end
+	start:
+		if (carry) begin
+			bitCnt <= 0;
+			out <= 0;
+			state <= data;
+		end
+	data:
+		if (carry) begin
+			out <= {out[6:0], rx};
+			bitCnt <= bitCnt + 1'b1;
+			state <= stop;
+		end
+	default: // stop
+		if (subCnt[3]) state <= idle;
+	endcase
+end
 
 endmodule
